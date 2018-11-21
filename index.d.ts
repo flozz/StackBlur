c9b7a2a2a55808bac166dcf788ab52958b3d5e7a
Jose Peleteiro <jose@peleteiro.net>
2018-11-21 08:40:36 -0200

    Typescript definition updated

--- index.d.ts
+++ index.d.ts
@@ -1,33 +1,50 @@
-declare namespace StackBlur {
-    function image(
-        img: HTMLImageElement | string,
-        canvas: HTMLCanvasElement | string,
-        radius: number,
-        blurAlphaChannel?: boolean): void;
-
-    function canvasRGBA(
-        canvas: HTMLCanvasElement,
-        topX: number, topY: number,
-        width: number, height: number,
-        radius: number): void;
-
-    function canvasRGB(
-        canvas: HTMLCanvasElement,
-        topX: number, topY: number,
-        width: number, height: number,
-        radius: number): void;
-
-    function imageDataRGBA(
-        data: ImageData,
-        topX: number, topY: number,
-        width: number, height: number,
-        radius: number): ImageData;
-
-    function imageDataRGB(
-        data: ImageData,
-        topX: number, topY: number,
-        width: number, height: number,
-        radius: number): ImageData;
+export class BlurStack {
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+  next: BlurStack;
 }
 
-export = StackBlur;
+export function image(
+  img: HTMLImageElement | string,
+  canvas: HTMLCanvasElement | string,
+  radius: number,
+  blurAlphaChannel?: boolean
+): void;
+
+export function canvasRGBA(
+  canvas: HTMLCanvasElement,
+  topX: number,
+  topY: number,
+  width: number,
+  height: number,
+  radius: number
+): void;
+
+export function canvasRGB(
+  canvas: HTMLCanvasElement,
+  topX: number,
+  topY: number,
+  width: number,
+  height: number,
+  radius: number
+): void;
+
+export function imageDataRGBA(
+  data: ImageData,
+  topX: number,
+  topY: number,
+  width: number,
+  height: number,
+  radius: number
+): ImageData;
+
+export function imageDataRGB(
+  data: ImageData,
+  topX: number,
+  topY: number,
+  width: number,
+  height: number,
+  radius: number
+): ImageData;
