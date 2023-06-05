afbf35d04977b96dbd0ac1f8f29b31b747d966b5
Jason11Q <127638883+Jason11Q@users.noreply.github.com>
2023-06-05 11:42:36 +0800

    Format stackblur.js

--- src/stackblur.js
+++ src/stackblur.js
@@ -91,14 +91,14 @@ const shgTable = [
  */
 function processImage (
   img, canvas, radius, blurAlphaChannel, useOffset, skipStyles
-) {  
+) {
   if (typeof img === 'string') {
     img = document.getElementById(img);
   }
 
   if (!img || (Object.prototype.toString.call(img).slice(8, -1) === 'HTMLImageElement' && !('naturalWidth' in img))) {
     return;
-  } 
+  }
 
   const dimensionType = useOffset ? 'offset' : 'natural';
   const w = img[dimensionType + 'Width'];
